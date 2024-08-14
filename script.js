a1a46bf7966361dcc418dec8e3fdcbd7262fdbfd
WebMohit20 <2003mohitkhemani@gmail.com>
2024-08-14 23:17:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+<header class="header">
+		<h1>Shaw's News</h1>
+	</header>
+	<div class="container">
+		<div class="latest-news-section">
+			<h3>Latest News Articles</h3>
+			<ul>
+				<li>Article 1</li>
+				<li>Article 2</li>
+				<li>Article 3</li>
+			</ul>
+		</div>
+		<div class="main-article">
+			<h2>Main Article</h2>
+			<p>
+				lorem ipsum dolo rety qwerty conseculator wold coldf qwedvg scdfg rtyj
+				asfgg ghjyi ertgyh cvbnj fthui mogfjnk dkjfghkjdcjlk snvkxnk 
+			</p>
+		</div>
+		<div class="pictorial-news-section">
+			<h3>Pictorial News</h3>
+			
+		</div>
